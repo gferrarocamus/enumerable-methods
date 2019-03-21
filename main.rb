bfb6a57fade8d79549d1ebaa3c60eda00a8a4c4e
Saheed <sdrfngg640@gmail.com>
2019-03-21 11:49:25 +0100

    creates my_each_with_index

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 module Enumerable
     def my_each
       if self.is_a? Array
-        for i in self
+        for i in 0..self.length-1
             yield self[i]
         end
       elsif self.is_a? Hash
@@ -10,6 +10,19 @@ module Enumerable
         end
       end
     end
+
+    def my_each_with_index
+      if self.is_a? Array
+        for i in 0..self.length-1 do
+          yield(self[i], i)
+        end
+      elsif self.is_a? Hash
+        for k in 0..self.keys.length-1
+          yield([self.keys[k], self.values[k]], k)
+        end
+      end
+    end
+    
 end
 
 arr=[1,2,3,4,5,6]
@@ -20,8 +33,9 @@ hash = {
     "c"=>3
 }
 
-hash.my_each do |item, v|
-    puts "#{item}, #{v}"
+hash.my_each_with_index do |item, a, b|
+    puts "#{item} #{a} #{b}"
 end
 
-"string".each {|c| puts c}
+
+#"string".each {|c| puts c}
